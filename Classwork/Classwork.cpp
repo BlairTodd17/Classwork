4336f4823f0c40d27b493d99bdea60b992c10a63
Blair Todd <bjbbt4@outlook.com>
2024-09-25 18:30:53 -0400

    ww

--- Classwork/Classwork.cpp
+++ Classwork/Classwork.cpp
@@ -1,20 +1,14 @@
-// Classwork.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//Starting point for the program
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    
+    int age = 18;
+    float weight = 165; 
+  
+    cout << "My age is " << age << " and my weight is " << weight << " pounds." << endl;
+
+    cout << "Press any key to exit..." << endl;
+    cin.get();
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
